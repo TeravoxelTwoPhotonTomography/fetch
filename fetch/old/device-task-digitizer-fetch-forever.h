9635ede91e7fec3f27a946e85ee7b73aa8110f53
Nathan Clack <clackn@janelia.hhmi.org>
2010-09-30 15:43:55 -0400

    First big move

--- fetch/old/device-task-digitizer-fetch-forever.h
+++
@@ -1,4 +0,0 @@
-#include "stdafx.h"
-#include "device-task.h"
-
-DeviceTask* Digitizer_Create_Task_Fetch_Forever(void);
--- fetch/old/device-task-scanner-video.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "stdafx.h"
-#include "device-task.h"
-#include "device-scanner.h"
-
-DeviceTask* Scanner_Create_Task_Video(void);
-DeviceTask* Scanner_Create_Task_Line_Scan(void);
-
-void DeviceTask_Scanner_Video_Write_Waveforms( Scanner *scanner );
