c39b0a5adb3902c5d07026455c4194aa71fa388f
Nathan Clack <clackn@janelia.hhmi.org>
2012-01-25 10:37:02 -0500

    Updated chan docs

--- src/chan.h
+++ src/chan.h
@@ -1,30 +1,25 @@
 /** \file
-    \ref Chan is a zero-copy multi-producer mutli-consumer asynchronous queue
-    for passing messages between threads.
+    \ref Chan is a cross-platfrom zero-copy multi-producer mutli-consumer
+    asynchronous queue for passing messages between threads.
 
     Reading and writing to the queue are both performed via Chan_Next().
     Whether a "read" or a "write" happens depends on the \a mode passed to
     Chan_Open(), which returns a reference-counted "reader" or "writer"
     reference to the queue.
-    
+
     Note that "opening" a channel is different than "allocating" it.  A \ref
     Chan is typically allocated once, but may be opened and closed many times.
     Chan_Alloc() initializes and reserves memory for the queue.  Internally,
     Chan_Open() does little more than some bookkeeping.
     
     \ref Chan was designed so that Chan_Next() operations block the calling
-    thread under some circumstances.  A read will wait for data if as long as a
-    writer has been opened for that \ref Chan elsewhere.  When data becomes
-    available, the read will return successfully returning a message from the
-    queue.  If the last writer is closed (Chan_Close()), the read returns an
-    error.  Writes work similarly by blocking until space is available on the
-    queue as long as readers are available.
+    thread under some circumstances.  See \ref next for details.  With proper
+    use, this design garauntees that certain networks of threads (namely,
+    directed acyclic graphs) connected by \ref Chan instances will deliver
+    every message emited from a producer to a consumer in that network.  Once
+    the network is assembled, messages will be recieved in topological order.
 
-    With proper use, this design garauntees that certain networks of threads
-    (namely, directed acyclic graphs) connected by \ref Chan instances will
-    deliver every message emited from a producer to a consumer in that network.
-    Once the network is assembled, messages will be recieved in topological
-    order.
+    \section ex Example producer/consumer
 
     An example producer thread might look something like this:
     \code
@@ -32,7 +27,7 @@
     { void  *data = Chan_Token_Buffer_Alloc(q);
       size_t nbytes;
       Chan  *output = Chan_Open(q,CHAN_WRITE);
-      while( data=getdata(&nbytes) )
+      while( getdata(&data,&nbytes) )
         if( CHAN_FAILURE( Chan_Next(output,&data,&nbytes)))
           break;
       Chan_Close(output);
@@ -40,8 +35,37 @@
     }
     \endcode
 
-    \author Nathan Clack <clackn@janelia.hhmi.org>
-    \date   Feb. 2011
+    An example consumer might look something like this:
+    \code
+    void consumer(Chan *q)
+    { void *data = Chan_Token_Buffer_Alloc(q);
+      size_t nbytes;
+      Chan *input = Chan_Open(q,CHAN_READ);
+      while( CHAN_SUCCESS( Chan_Next(input,&data,&nbytes)))
+        dosomething(data,nbytes);
+      Chan_Close(input);
+      Chan_Token_Buffer_Free(data);
+    }
+    \endcode
+
+    Both the \c producer() and \c consumer() take a \ref Chan*, \c q, as input.
+    This is the communication channel that will link the two threads.  Both
+    functions start by using Chan_Token_Buffer_Alloc() to allocate space for
+    the first message.  It isn't neccessary to call this in \c producer(), but
+    it is a recommended pattern.  See \ref mem for details.
+
+    The \c producer() then opens a \ref CHAN_WRITE reference to \c q, and
+    generates data using the mysterious \c getdata() procedure that fills the 
+    \c data buffer with something interesting.  Note that \c getdata() might
+    change or reallocate the \c data pointer.
+
+    It then pushes \c data onto the queue with Chan_Next().  Pushing just swaps
+    the address of \c data with the address of an unused buffer, so pushing can
+    be as fast as swapping two pointers.  In the example, if the push fails,
+    the \c producer() will clean up and terminate.  Techinically the push can
+    only fail here if something goes wrong with the under-the-hood
+    synchronization.
+
   */
 #pragma once
 #include <stdlib.h>
@@ -73,47 +97,46 @@ void     Chan_Wait_For_Writer_Count ( Chan* self,size_t n);
 void     Chan_Wait_For_Have_Reader( Chan* self);
 void     Chan_Set_Expand_On_Full ( Chan* self, int  expand_on_full);                           // default: no expand
 
-//
-// ----
-// Next
-// ----
-//
-// Requires a mode to be set.
-// For Chan's open in read mode, does a pop.  Reads will fail if there
-//     are no writers for the channel and the channel is empty.
-//     Otherwise, when the channel is empty, reads will block.
-// For chan's open in write mode, does a push.  Writes will block when
-//     the channel is full.
-//
-//   *    Overflow                       Underflow
-// =====  ============================   ===================
-// -      Waits or expands.              Fails if no sources, otherwise waits.
-// Copy   Waits or expands.              Fails if no sources, otherwise waits.
-// Try    Fails immediately              Fails immediately.
-// Timed  Waits.  Fails after timeout.   Fails immediately if no sources, otherwise waits till timeout.
-//
+/** \file
+    \section next Next Functions
+  
+    These require a mode to be set via Chan_Open().
+    For \ref Chan's open in read mode, does a pop.  Reads will fail if there
+        are no writers for the \ref Chan and the \ref Chan is empty.
+        Otherwise, when the \ref Chan is empty, reads will block.
+    For \ref Chan's open in write mode, does a push.  Writes will block when
+        the \ref Chan is full.
 
-unsigned int Chan_Next         ( Chan *self,  void **pbuf, size_t sz); ///< Pops next item. Returns failure if there are no writers, otherwise waits.
-unsigned int Chan_Next_Copy    ( Chan *self,  void  *buf,  size_t sz); ///< Pops a copy of the next item.  Returns failure if there are no writers, otherwise waits.
-unsigned int Chan_Next_Try     ( Chan *self,  void **pbuf, size_t sz); ///< If no item's are on the queue, return failure.  Otherwise, pop the next item.
-unsigned int Chan_Next_Copy_Try( Chan *self_, void  *buf,  size_t sz); ///< If no item's are on the queue, return failure.  Otherwise, pop a copy of the next item. 
+    \verbatim
+      *    Overflow                       Underflow
+    =====  ============================   ===================
+    -      Waits or expands.              Fails if no sources, otherwise waits.
+    Copy   Waits or expands.              Fails if no sources, otherwise waits.
+    Try    Fails immediately              Fails immediately.
+    Timed  Waits.  Fails after timeout.   Fails immediately if no sources, otherwise waits till timeout.
+    \endverbatim
+*/
+
+unsigned int Chan_Next         ( Chan *self,  void **pbuf, size_t sz); ///< Push or pop next item.  May block the calling thread.
+unsigned int Chan_Next_Copy    ( Chan *self,  void  *buf,  size_t sz); ///< Push or pop a copy.  May block the calling thread.  
+unsigned int Chan_Next_Try     ( Chan *self,  void **pbuf, size_t sz); ///< Push (or pop), but never block.  If the queue is full (or empty) immediately return failure.
+unsigned int Chan_Next_Copy_Try( Chan *self_, void  *buf,  size_t sz); ///< Same as Chan_Next_Try(), but pushes or pops a copy.  Will not block.
 unsigned int Chan_Next_Timed   ( Chan *self,  void **pbuf, size_t sz,   unsigned timeout_ms ); ///< Just like Chan_Next(), but any waiting is limited by the timeout.
 
-// ----
-// Peek
-// ----
-//
-// Requires read mode.
-//
+/** \file
+    \section peek Peek Functions
+
+    Requires read mode.
+*/ 
 unsigned int Chan_Peek       ( Chan *self, void **pbuf, size_t sz );
 unsigned int Chan_Peek_Try   ( Chan *self, void **pbuf, size_t sz );
 unsigned int Chan_Peek_Timed ( Chan *self, void **pbuf, size_t sz, unsigned timeout_ms );
 
-// -----------------
-// Memory management
-// -----------------
-//
-// Resize: when nbytes is less than current size, does nothing
+/** \file
+    \section mem Memory management
+ 
+    Resize: when nbytes is less than current size, does nothing
+*/ 
 
 int         Chan_Is_Full                    ( Chan *self );
 int         Chan_Is_Empty                   ( Chan *self );
@@ -132,4 +155,7 @@ extern size_t Chan_Buffer_Count             ( Chan *self );
 #ifdef __cplusplus
 }
 #endif
-
+/** \file
+    \author Nathan Clack <clackn@janelia.hhmi.org>
+    \date   Feb. 2011
+*/
